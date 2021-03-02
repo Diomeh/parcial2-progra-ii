1200e6f3037858d344cd001ac5df6ae215294e04
David Urbina <duparra15@gmail.com>
2021-03-02 13:41:34 -0400

    Implementado tablero

--- index.php
+++ index.php
@@ -7,49 +7,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Buscaminas</title>
 
-    <!-- jQery para poder usar AJAX -->
+    <!-- jQery to be able to make AJAX calls -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <h2>BUSCAMINAS</h2>
-    <?php if (empty($_POST['rows']) || empty($_POST['cols'])) { ?>
+    <?php if (empty($_POST['size'])) { ?>
         <form action="" method="post">
-            <input type="number" name="rows" id="rows" placeholder="Filas">
-            <br>
-            <input type="number" name="cols" id="cols" placeholder="Columnas">
-            <br>
-            <button type="submit">Crear</button>
+            <input type="number" name="size" id="size" placeholder="Tamaño: ">
+            <button type="submit">Jugar</button>
         </form>
-    <?php } else { ?>
-        <table>
-            <thead>
-                <tr>
-                    <!-- <th>table</th> -->
-                </tr>
-            </thead>
-
-            <tbody>
-                <?php for ($i = 0; $i < $_POST['rows']; $i++) { ?>
-                    <tr>
-                        <?php for ($j = 0; $j < $_POST['cols']; $j++) { ?>
-                            <td>
-                                <button onclick="handleTileClicked()"></button>
-                            </td>
-                        <?php } ?>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+    <?php } else {
+        include_once __DIR__ . "/Board.php";
+        session_start();
+        $board = $_SESSION['board'] = new Board($_POST['size']);
+        echo $board;
+    ?>
         <br>
-        <a href="/public">Reinciciar</a>
+        <a href="/parcial2">Reinciciar</a>
     <?php } ?>
 
     <!-- Calls a php script every time a button tile is clicked -->
     <script type="text/javascript">
-        function handleTileClicked() {
-            // $.post('page.php');
-            console.log('jejejej')
+        function handleTileClicked(id) {
+            console.log(id);
             return false;
         }
     </script>
